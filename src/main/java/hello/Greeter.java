99b6c021120d7652d13c6514b434782a8f3c94b0
SwanandTiwatane <157562089+SwanandTiwatane@users.noreply.github.com>
2024-03-20 09:39:51 +0530

    Added new changes

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Hi DevOps Project for Technogeeks Technologies";
+        return "Hi DevOps Project for Technogeeks jenkins Technologies";
     }
 }
